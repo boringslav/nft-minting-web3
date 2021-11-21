a964a4127905ac4b8a0651690558c293d21d8f24
Borislav Stoyanov <bostoy@protonmail.com>
2021-11-21 14:21:02 +0200

    Initialize frontend + configure tailwind css

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div class="h-screen bg-gray-700 flex-row justiy-items-center items-center ">
+      <button class="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-pink-500 hover:bg-pink-700">
+        NFTs
+      </button>
+    </div>
+
+  );
+}
+
+export default App;
