0a83bb12715734d078e7b66bfb51a2d327403efa
Borislav Stoyanov <bostoy@protonmail.com>
2021-11-21 12:41:28 +0200

    create nft json and base64 encode it

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -5,6 +5,9 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
+// We need to import the helper functions from the contract that we copy/pasted.
+import {Base64} from "./libraries/Base64.sol";
+
 contract NFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
@@ -13,49 +16,90 @@ contract NFT is ERC721URIStorage {
     string[] secondWords = ["Crocodile", "Iguana", "Lion", "Rat"];
     string[] thirdWords = ["Bicycle", "Cabrio", "Tank", "Taxi"];
 
-
     // This is our SVG code. All we need to change is the word that's displayed. Everything else stays the same.
     // So, we make a baseSvg variable here that all our NFTs can use.
-    string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
-
+    string baseSvg =
+        "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
     // We need to pass the name of our NFTs token and it's symbol.
-    constructor() ERC721("SlavNFT", "SNFT"){
+    constructor() ERC721("SlavNFT", "SNFT") {
         console.log("First contract...");
     }
 
-    function pickRandomWord(string[] memory _wordArray, uint _tokenId, string memory _randomWord) public pure returns(string memory) {
-        uint rand = random(string(abi.encodePacked(_randomWord, Strings.toString(_tokenId))));
+    function pickRandomWord(
+        string[] memory _wordArray,
+        uint256 _tokenId,
+        string memory _randomWord
+    ) public pure returns (string memory) {
+        uint256 rand = random(
+            string(abi.encodePacked(_randomWord, Strings.toString(_tokenId)))
+        );
         rand = rand % _wordArray.length;
         return _wordArray[rand];
-
     }
 
-    function random(string memory input) internal pure returns(uint) {
-        return uint(keccak256(abi.encodePacked(input)));
+    function random(string memory input) internal pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(input)));
     }
 
     function createNFT() public {
-        uint newTokenId = _tokenIds.current();
-        string memory first = pickRandomWord(firstWords, newTokenId, "fiiiirst");
-        string memory second = pickRandomWord(secondWords, newTokenId, "secooond");
-        string memory third = pickRandomWord(thirdWords, newTokenId, "thhhiiird");
+        uint256 newTokenId = _tokenIds.current();
+        string memory first = pickRandomWord(
+            firstWords,
+            newTokenId,
+            "fiiiirst"
+        );
+        string memory second = pickRandomWord(
+            secondWords,
+            newTokenId,
+            "secooond"
+        );
+        string memory third = pickRandomWord(
+            thirdWords,
+            newTokenId,
+            "thhhiiird"
+        );
+        string memory finalWord = string(
+            abi.encodePacked(first, second, third)
+        );
 
-        string memory finalSvg = string(abi.encodePacked(baseSvg,first,second,third,"</text></svg>"));
-        console.log('FinalSvg: ', finalSvg);
+        string memory finalSvg = string(
+            abi.encodePacked(baseSvg, first, second, third, "</text></svg>")
+        );
+        console.log("FinalSvg: ", finalSvg);
+
+        // Get all the JSON metadata in place and base64 encode it.
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        // We set the title of our NFT as the generated word.
+                        finalWord,
+                        '", "description": "A highly acclaimed collection of slavs", "image": "data:image/svg+xml;base64,',
+                        // We add data:image/svg+xml;base64 and then append our base64 encode our svg.
+                        Base64.encode(bytes(finalSvg)),
+                        '"}'
+                    )
+                )
+            )
+        );
+
+        // Just like before, we prepend data:application/json;base64, to our data.
+        string memory finalTokenUri = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+
+        console.log("Token Uri", finalTokenUri);
 
         _safeMint(msg.sender, newTokenId);
 
-        /*
-        {title, description,image}
-         base64 encode the image, after that base64 encode the entire json object
-         */
-        _setTokenURI(newTokenId, "data:application/json;base64,eyJ0aXRsZSI6ImJvcmluZ3NsYXYiLCJkZXNjcmlwdGlvbiI6Im1lIiwiaW1hZ2UiOiJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJaeUI0Yld4dWN6MGlhSFIwY0RvdkwzZDNkeTUzTXk1dmNtY3ZNakF3TUM5emRtY2lJSEJ5WlhObGNuWmxRWE53WldOMFVtRjBhVzg5SW5oTmFXNVpUV2x1SUcxbFpYUWlJSFpwWlhkQ2IzZzlJakFnTUNBek5UQWdNelV3SWo0S0lDQWdJRHh6ZEhsc1pUNHVZbUZ6WlNCN0lHWnBiR3c2SUhkb2FYUmxPeUJtYjI1MExXWmhiV2xzZVRvZ2MyVnlhV1k3SUdadmJuUXRjMmw2WlRvZ01UUndlRHNnZlR3dmMzUjViR1UrQ2lBZ0lDQThjbVZqZENCM2FXUjBhRDBpTVRBd0pTSWdhR1ZwWjJoMFBTSXhNREFsSWlCbWFXeHNQU0ppYkdGamF5SWdMejRLSUNBZ0lEeDBaWGgwSUhnOUlqVXdKU0lnZVQwaU5UQWxJaUJqYkdGemN6MGlZbUZ6WlNJZ1pHOXRhVzVoYm5RdFltRnpaV3hwYm1VOUltMXBaR1JzWlNJZ2RHVjRkQzFoYm1Ob2IzSTlJbTFwWkdSc1pTSStZbTl5YVc1bmMyeGhkand2ZEdWNGRENEtQQzl6ZG1jKyJ9");
-        console.log("An NFT /w ID %s has been minted to %s", newTokenId ,msg.sender);
+        _setTokenURI(newTokenId, finalTokenUri);
+        console.log(
+            "An NFT /w ID %s has been minted to %s",
+            newTokenId,
+            msg.sender
+        );
         _tokenIds.increment();
     }
-
-
-
-
 }
