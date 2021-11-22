1a2505bd52cb1268a852b48943c6bb66d6c6146a
Borislav Stoyanov <bostoy@protonmail.com>
2021-11-22 11:42:41 +0200

    add abi file to /utils dir + mint an nft from fe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
+import { ethers } from "ethers";
 import { useState, useEffect } from "react";
+import myNFT from './utils/NFT.json'; //get the abi file from artficts and add it to /utils dir
 
 function App() {
   const [currentAccount, setCurrentAccount] = useState("");
-
+  const CONTRACT_ADDRESS = "0x064a65144Dd209756346254FdB2eD6577d69dF97";
   useEffect(() => {
     checkIfWalletIsConnected();
   }, []);
@@ -44,9 +46,36 @@ function App() {
 
       setCurrentAccount(accounts.at(0));
     } catch (err) {
-      console.err(err);
+      console.error(err);
     }
   };
+
+  const askContractToMintNFT = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myNFT.abi, signer);
+
+        console.log("Going to pop wallet now to pay gas...");
+
+        let nftTxn = await connectedContract.createNFT();
+
+        console.log("Mining...please wait.")
+        await nftTxn.wait();
+
+        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+
+    } catch (err) {
+      console.error(err);
+    }
+
+  }
 
   const renderNotConnectedContainer = () => (
     <button
@@ -71,7 +100,7 @@ function App() {
             renderNotConnectedContainer()
           ) : (
             <button
-              onClick={null}
+              onClick={askContractToMintNFT}
               class="mt-5 p-3 border-2 border-pink-400 rounded-lg font-bold text-white text-xxl cursor-pointer"
             >
               Mint NFT
