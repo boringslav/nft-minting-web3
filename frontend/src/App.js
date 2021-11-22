1847091ab9c717893435202c2ed80b6ac1fcb687
Borislav Stoyanov <bostoy@protonmail.com>
2021-11-22 10:01:57 +0200

    connect metamask wallet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,85 @@
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div class="h-screen bg-gray-700 flex-row justiy-items-center items-center ">
-      <button class="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-pink-500 hover:bg-pink-700">
-        NFTs
-      </button>
-    </div>
+  const [currentAccount, setCurrentAccount] = useState("");
 
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    }
+    console.log("We have the ethereum object", ethereum);
+
+    // Check if we're authorized to access the user's wallet
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    //User can have multiple authorized accounts, we grab the first one if its there!
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      // setCurrentAccount(account)
+    } else {
+      console.log("No authorized account found");
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) alert("Get MetaMask!");
+
+      // Request access to ethereum account
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      accounts.at(0);
+
+      console.log("Connected", accounts.at(0));
+
+      setCurrentAccount(accounts.at(0));
+    } catch (err) {
+      console.err(err);
+    }
+  };
+
+  const renderNotConnectedContainer = () => (
+    <button
+      onClick={connectWallet}
+      class="mt-5 p-3 border-2 border-pink-400 rounded-lg font-bold text-white text-xxl cursor-pointer"
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  return (
+    <div class="h-screen w-screen bg-gray-700 overflow-y-auto">
+      <div class="h-full flex flex-row justify-center  w-full">
+        <div class="pt-4">
+          <p class="m-0 p-3 text-4xl font-bold text-pink-400">
+            My NFT Collection
+          </p>
+          <p class="px-2 text-xl text-white ">
+            Each unique. Each beautiful. Discover your NFT today.
+          </p>
+          {currentAccount === "" ? (
+            renderNotConnectedContainer()
+          ) : (
+            <button
+              onClick={null}
+              class="mt-5 p-3 border-2 border-pink-400 rounded-lg font-bold text-white text-xxl cursor-pointer"
+            >
+              Mint NFT
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
 
